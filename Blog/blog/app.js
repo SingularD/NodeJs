9a10c152b2eb2ab8a49da97b5993a5c28215a6fc
SingularD <906229740@qq.com>
2018-05-31 21:22:52 +0800

    blog

--- Blog/blog/app.js
+++ Blog/blog/app.js
@@ -5,9 +5,10 @@
 
 var express = require('express');
 var routes = require('./routes');
-// var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var MongoStore = require('connect-mongo')(express);
+var settings = require('./settings');
 
 var app = express();
 
@@ -20,6 +21,15 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.session({
+    secret: settings.cookieSecret,
+    key: settings.db,
+    cookie: {maxAge: 1000*60*60*24*30},
+    store: new MongoStore({
+        db: settings.db
+    })
+}))
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
