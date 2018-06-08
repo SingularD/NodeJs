364c2742d0a4190e7ae797d0fa1e324106b4c7a8
SingularD <906229740@qq.com>
2018-06-08 23:49:01 +0800

    upload

--- Blog/blog/routes/index.js
+++ Blog/blog/routes/index.js
@@ -40,7 +40,7 @@ module.exports = function (app) {
             return res.redirect('/reg');
         }
         var md5 = crypto.createHash('md5'),
-            password = md5.update(req.body.password).digest('hex');
+            password = md5.update((req.body.password).toString()).digest('hex');
         var newUser = new User({
             name: req.body.name,
             password: password,
@@ -74,13 +74,15 @@ module.exports = function (app) {
     app.post('/login',checkNotLogin);
     app.post('/login',function (req,res) {
         var md5 = crypto.createHash('md5'),
-            password = md5.update(req.body.password).digest('hex');
+            password = md5.update((req.body.password).toString()).digest('hex');
         User.get(req.body.name,function (err,user) {
             if (!user) {
                 req.flash('error','用户不存在!');
                 return res.redirect('/login');
             }
-            if (user.password === password) {
+            console.log(password);
+            console.log(user.password);
+            if (!(user.password === password)) {
                 req.flash('error','密码错误！');
                 return res.redirect('/login')
             }
@@ -122,6 +124,15 @@ module.exports = function (app) {
         res.redirect('/');
     });
 
+    app.get('/upload',checkLogin);
+    app.post('/upload',function (req,res) {
+       res.render('upload',{
+           title: '上传',
+           user: req.session.user,
+           success: req.flash('success').toString(),
+           error: req.flash('error').toString()
+       });
+    });
     function checkLogin(req,res,next) {
         if (!req.session.user){
             req.flash('error','未登录');
