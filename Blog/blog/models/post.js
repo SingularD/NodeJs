e87950238d780fc82dcae568ac07d7cf72e9a637
SingularD <906229740@qq.com>
2018-06-13 21:15:51 +0800

    PV统计

--- Blog/blog/models/post.js
+++ Blog/blog/models/post.js
@@ -1,10 +1,11 @@
 var mongodb = require('./db');
 var markdown = require('markdown').markdown;
 
-function Post(name,title,post) {
+function Post(name,title,tags,post) {
     this.name = name;
     this.title = title;
-    this.post = post
+    this.tags = tags;
+    this.post = post;
 }
 
 module.exports = Post;
@@ -23,8 +24,10 @@ Post.prototype.save = function (callback) {
         name: this.name,
         time: time,
         title: this.title,
+        tags: this.tags,
         post: this.post,
-        comments: []
+        comments: [],
+        pv: 0
     };
     mongodb.open(function (err,db) {
         if (err) {
@@ -102,12 +105,24 @@ Post.getOne = function(name, day, title, callback) {
                 "time.day": day,
                 "title": title
             }, function (err, doc) {
-                mongodb.close();
                 if (err) {
+                    mongodb.close();
                     return callback(err);
                 }
                 //解析 markdown 为 html
                 if (doc) {
+                    collection.update({
+                        "name": name,
+                        "time.day": day,
+                        "title": title
+                    },{
+                        $inc: {"pv": 1}
+                    },function (err) {
+                        mongodb.close();
+                        if (err) {
+                            return callback(err);
+                        }
+                    });
                     doc.post = markdown.toHTML(doc.post);
                     doc.comments.forEach(function (comment) {
                         comment.content = markdown.toHTML(comment.content);
@@ -211,6 +226,56 @@ Post.getArchive = function (callback) {
                 "name": 1,
                 "time": 1,
                 "title": 1
+            }).sort({
+                time: -1
+            }).toArray(function (err,docs) {
+                mongodb.close();
+                if (err) {
+                    return callback(err);
+                }
+                callback(null,docs);
+            });
+        });
+    });
+};
+
+Post.getTags = function (callback) {
+    mongodb.open(function (err,db) {
+        if (err) {
+            return callback(err);
+        }
+        db.collection('posts',function (err,collection) {
+            if (err) {
+                mongodb.close();
+                return callback(err);
+            }
+            collection.distinct("tags",function (err,docs) {
+                mongodb.close();
+                if (err) {
+                    return callback(err);
+                }
+                callback(null,docs);
+            });
+        });
+    });
+};
+
+Post.getTag = function (tag,callback) {
+    mongodb.open(function (err,db) {
+        if (err) {
+            return callback(err);
+        }
+        db.collection('posts',function (err,collection) {
+            if (err) {
+                mongodb.close();
+                return callback(err);
+            }
+            collection.find({
+                "tags":tag
+            },{
+                "name":1,
+                "time":1,
+                "title":1
             }).sort({
                 time: -1
             }).toArray(function (err,docs) {
